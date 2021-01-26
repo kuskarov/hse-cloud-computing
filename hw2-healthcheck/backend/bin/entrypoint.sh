f731c5fce8af69946ccf2193fdafeea51a3dcf99
Tagir Kuskarov <kuskarov@smartdec.com>
2021-01-26 06:53:43 +0300

    Use gunicorn and .env files

--- hw2-healthcheck/backend/bin/entrypoint.sh
+++ hw2-healthcheck/backend/bin/entrypoint.sh
@@ -3,15 +3,17 @@
 if [ "$DATABASE" = "postgres" ]; then
   echo "Waiting for postgres..."
 
-  while ! nc -z $SQL_HOST $SQL_PORT; do
+  while ! nc -z "$SQL_HOST" "$SQL_PORT"; do
     sleep 0.1
   done
 
   echo "PostgreSQL started"
 fi
 
-echo "Creating database ..."
-python manage.py create_db
-echo "Created database"
+if [ "$FLASK_ENV" = "development" ]; then
+  echo "Creating the database tables..."
+  python manage.py create_db
+  echo "Tables created"
+fi
 
 exec "$@"
